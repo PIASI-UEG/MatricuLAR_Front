404519bb98260a27c5a189a7b49d673ecb802b2e
felipeKauaBarros <fkb475@gmail.com>
2024-03-23 20:23:21 -0300

    home_usuário

--- src/app/pages/home-page/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page/home-page.component.ts
@@ -9,12 +9,13 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class HomePageComponent {
     images = [
-        'assets/fotos_home/imagem-home.jpeg',
-        'assets/fotos_home/imagem2-home.jpeg'
+        'assets/fotos_home/imagem1-home.jpeg',
+        'assets/fotos_home/imagem2-home.jpeg',
+        'assets/fotos_home/imagem3-home.jpeg'
     ];
 
     constructor(config: NgbCarouselConfig) {
-        config.interval = 3000; 
+        config.interval = 3000;
         config.wrap = true;
         config.keyboard = false;
         config.pauseOnHover = false;
