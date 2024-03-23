bd1f6f103d4d474434b957e2ac216befe50605ea
felipeKauaBarros <fkb475@gmail.com>
2024-03-23 18:23:05 -0300

    atualização no home do usuário

--- src/app/pages/home-page/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page/home-page.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
-import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss'],
-  providers: [NgbCarouselConfig],
+    selector: 'app-home',
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.scss'],
+    providers: [NgbCarouselConfig],
 })
 export class HomePageComponent {
-  images = [700, 533, 807, 124].map((n) => 'assets/fotos_home/imagem-home.jpeg');
+    images = [
+        'assets/fotos_home/imagem-home.jpeg',
+        'assets/fotos_home/imagem2-home.jpeg'
+    ];
 
-  constructor(config: NgbCarouselConfig) {
-    config.interval = 3000;
-    config.wrap = true;
-    config.keyboard = false;
-    config.pauseOnHover = false;
-  }
+    constructor(config: NgbCarouselConfig) {
+        config.interval = 3000; 
+        config.wrap = true;
+        config.keyboard = false;
+        config.pauseOnHover = false;
+    }
 }
