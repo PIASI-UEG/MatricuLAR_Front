e1839180215413374619882a4d404e0d7143b15f
felipeKauaBarros <fkb475@gmail.com>
2024-03-23 23:31:19 -0300

    senha-atualizada-com-sucesso

--- src/app/pages/esqueceu-senha/esqueceu-senha/esqueceu-senha.component.ts
+++ src/app/pages/esqueceu-senha/esqueceu-senha/esqueceu-senha.component.ts
@@ -1,76 +1,47 @@
-import {Component, OnInit} from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { UsuarioDto } from "../../../api/models/usuario-dto";
-import { Observable } from "rxjs";
-import { UsuarioSenhaDto } from "../../../api/models/usuario-senha-dto";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import { DateAdapter } from "@angular/material/core";
-import { UsuarioControllerService } from "../../../api/services/usuario-controller.service";
-import { ActivatedRoute, Router } from "@angular/router";
-import { MatDialog } from "@angular/material/dialog";
-import { SecurityService } from "../../../arquitetura/security/security.service";
-import {Validacoes} from "../../../../Validacoes";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioDto } from '../../../api/models/usuario-dto';
+import { Validacoes } from '../../../../Validacoes';
+import { UsuarioControllerService } from '../../../api/services/usuario-controller.service';
 
 @Component({
     selector: 'app-esqueceu-senha',
     templateUrl: 'esqueceu-senha.component.html',
     styleUrls: ['./esqueceu-senha.component.scss']
 })
-export class EsqueceuSenhaComponent implements OnInit{
+export class EsqueceuSenhaComponent implements OnInit {
 
     formGroup!: FormGroup;
     validacoes: Validacoes = new Validacoes();
+    usuarioDTO: UsuarioDto[] = [];
 
-    constructor(private formBuilder: FormBuilder,
-                private _adapter: DateAdapter<any>,
-                private usuarioService: UsuarioControllerService,
-                private router: Router,
-                private route: ActivatedRoute,
-                private dialog: MatDialog,
-                private securityService: SecurityService,
-                private http: HttpClient) {
-        this._adapter.setLocale('pt-br');
-    }
-
-    createForm() {
-        this.formGroup = this.formBuilder.group({
-            cpf: [null, [Validators.required, this.validacoes.validarCpf]],
-            email: [null, Validators.required],
-        });
-    }
+    constructor(
+        private formBuilder: FormBuilder,
+        private usuarioController: UsuarioControllerService,
+    ) { }
 
     ngOnInit() {
         this.createForm();
     }
 
-    onSubmit() {
-
-    }
-
-    recuperarSenha(email: string, cpf: string): void {
-        this.verificarCPF(email, cpf).subscribe((usuario: UsuarioDto) => {
-            if (usuario) {
-                this.solicitarRedefinicaoSenha(email);
-            } else {
-                console.log('CPF não cadastrado.');
-            }
+    private createForm() {
+        this.formGroup = this.formBuilder.group({
+            cpf: [null, [Validators.required, this.validacoes.validarCpf]],
+            email: [null, [Validators.required, this.validacoes.validarEmail]],
         });
     }
 
-    verificarCPF(email: string, cpf: string): Observable<UsuarioDto> {
-        return this.http.get<UsuarioDto>('URL_VERIFICAR_CPF', { params: { email, cpf } });
+    onSubmit() {
+        const { email, cpf } = this.formGroup.value;
+        this.recuperarSenha(email, cpf);
     }
 
-    solicitarRedefinicaoSenha(email: string): void {
-        const novaSenhaAleatoria = this.gerarSenhaAleatoria();
-
-        const body: UsuarioSenhaDto = {
-            email,
-            novaSenha: novaSenhaAleatoria
-        };
-
-        this.http.post('URL_REDEFINIR_SENHA', body).subscribe(() => {
+    recuperarSenha(email: string, cpf: string): void {
+        const requestBody = { body: cpf }; // Construindo o corpo da requisição
+        this.usuarioController.usuarioControllerRedefinirSenha(requestBody).subscribe(() => {
             console.log('Solicitação de redefinição de senha enviada com sucesso.');
+        }, error => {
+            console.error('Erro ao enviar solicitação de redefinição de senha:', error);
         });
     }
 
