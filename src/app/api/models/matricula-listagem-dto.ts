9508ce74d4690bb956779af40578a75a0be15895
lucashspiess <lucashs725@gmail.com>
2024-04-26 22:46:40 -0300

    UPDATE escolher o tutor para gerar o termo

---
+++ src/app/api/models/matricula-listagem-dto.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export interface MatriculaListagemDto {
+  nomeAluno?: string;
+  nomeResponsaveis?: Array<string>;
+  nroMatricula?: number;
+  statusMatricula?: string;
+  telefoneResponsaveis?: Array<string>;
+  tituloTurma?: string;
+}
