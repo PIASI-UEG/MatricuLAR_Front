97248b2493581e82ae4071b1c560bd6c6f257c83
felipeKauaBarros <fkb475@gmail.com>
2024-07-09 19:10:15 -0300

    atualizacao

--- src/app/pages/funcionario/form-funcionario/form-funcionario.component.ts
+++ src/app/pages/funcionario/form-funcionario/form-funcionario.component.ts
@@ -60,24 +60,8 @@ export class FormFuncionarioComponent implements OnInit {
     this.cdr.detectChanges();
   }
 
-  validarSenhas() {
-    // Obtém os valores das senhas
-    const senha = this.formGroup.get('senha')?.value;
-    const confirmarSenha = this.formGroup.get('confirmarSenha')?.value;
-    // Verifica se as senhas são iguais
-
-    if (senha !== confirmarSenha && confirmarSenha && confirmarSenha !== '') {
-      // Adiciona um erro personalizado ao formulário
-      this.formGroup.get('confirmarSenha')?.setErrors({ 'naoConfere': true });
-      // Retorna falso
-      return false;
-    }
-    // Retorna verdadeiro
-    return true;
-  }
-
   private createForm() {
-    if(this.acao == "Editar" && this.codigo != null){
+    if(this.acao == "Editar"){
       this.usuarioService.usuarioControllerObterPorId({id: this.codigo as number}).subscribe(retorno =>
         this.formGroup = this.formBuilder.group({
           pessoaNome: [retorno.pessoaNome, [Validators.required, this.validacoes.validarCampoEmBranco, Validators.maxLength(200)]],
@@ -85,7 +69,10 @@ export class FormFuncionarioComponent implements OnInit {
           cargo: [retorno.cargo, Validators.required],
           email: [retorno.email, [Validators.required, this.validacoes.validarEmail, Validators.maxLength(100)]],
           pessoaTelefone: [retorno.pessoaTelefone, [Validators.required, this.validacoes.validarTelefone, Validators.maxLength(11)]],
-          idUsuarioRequisicao: [this.securityService.getUserId()]
+            senha: [null],
+            confirmarSenha: [null],
+            idUsuarioRequisicao: [this.securityService.getUserId()]
+
         }));
     }
     else {
@@ -110,10 +97,7 @@ export class FormFuncionarioComponent implements OnInit {
     return this.formGroup.controls[controlName].hasError(errorName);
   };
 
-  onSubmit() {
-      this.submitFormulario = true;
-
-     
+  onSubmit(){
 
       if (!this.codigo) {
           this.realizarInclusao();
